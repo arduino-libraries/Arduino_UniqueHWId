f5e546b50c9aede8a24792b0ed0490d5f78d8fc4
pennam <m.pennasilico@arduino.cc>
2025-02-20 15:05:40 +0100

    Add debug prints

--- src/uniqueHWId.cpp
+++ src/uniqueHWId.cpp
@@ -13,26 +13,33 @@
 #include "uniqueHWId.h"
 
 bool UniqueHWId::begin() {
-    _init =_sId.begin() && _nId.begin();
+    _init = (_sId.begin() && _nId.begin());
     return _init;
 }
 
 bool UniqueHWId::get(uint8_t* in, uint32_t size) {
     if (_init == false || size < BOARD_PROVISIONING_ID_SIZE) {
+        Serial.println("size ERROR");
         return false;
     }
     uint8_t offset = 0;
     if (!microId::get(&in[offset], size)) {
+        Serial.println("microId ERROR");
         return false;
     }
+    Serial.print("microId: ");Serial.println(THEXT::encode(&in[offset], UC_UID_SIZE));
     offset += UC_UID_SIZE;
     if (!_nId.get(&in[offset], size - offset)) {
+        Serial.println("networkId ERROR");
         return false;
     }
+    Serial.print("networkId: ");Serial.println(THEXT::encode(&in[offset], NETWORK_ID_SIZE));
     offset += NETWORK_ID_SIZE;
     if (!_sId.get(&in[offset], size - offset)) {
+        Serial.println("secureId ERROR");
         return false;
     }
+    Serial.print("secureId: ");Serial.println(THEXT::encode(&in[offset], CRYPTO_SN_SIZE));
     return true;
 }
 
@@ -41,6 +48,7 @@ String UniqueHWId::get() {
     if (!get(data, sizeof(data))) {
         return String("");
     }
+    Serial.print("preSHA: ");Serial.println(THEXT::encode(data, BOARD_PROVISIONING_ID_SIZE));
     uint8_t out[SHA256_DIGEST_SIZE];
     SHA256::sha256(data, sizeof(data), out);
     return THEXT::encode(out, sizeof(out));
